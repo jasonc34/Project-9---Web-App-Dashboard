c7b45b1a3b41fa2f8c948e55b0e4a635410ebae6
Jason Castillo <jasonc34@gmail.com>
2016-10-26 08:31:08 -0700

    Project - 9 Web App Dash
    Unfishished verison

---
+++ jquery/main.js
@@ -0,0 +1,18 @@
+function search(v) {
+  reset();
+
+  var out = document.querySelectorAll('img:not([alt*="' + v + '"])');
+  [].forEach.call(out, function(x) {
+    x.style.display = 'none';
+  });
+}
+
+function reset() {
+  [].forEach.call(document.querySelectorAll('img'), function(x) {
+    x.removeAttribute('style');
+  });
+}
+
+
+
+
